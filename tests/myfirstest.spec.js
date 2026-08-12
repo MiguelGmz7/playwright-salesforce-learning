a3d5af00c2701f4b8a288f8f506e94856ccf20b1
Miguel Gomez <mail@miguel-gomez.dev>
2026-08-12 02:31:57 -0600

    vault backup: 2026-08-12 02:31:56

--- tests/myfirstest.spec.js
+++ tests/myfirstest.spec.js
@@ -3,9 +3,10 @@ const {test} = require("@playwright/test");
 
 test('My First test', function(){
     //Playwright Code
-    //Step1 launch browser
+    //Step1 launch 
+    await 
     //Step2 Enter Credentials
     //Step3 Click Sign Button
 
-    
+
 });
