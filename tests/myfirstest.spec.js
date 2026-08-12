77861f04ab2f1a431d5241ee5f1f1a35d030a0a7
Miguel Gomez <mail@miguel-gomez.dev>
2026-08-12 02:41:06 -0600

    vault backup: 2026-08-12 02:41:05

--- tests/myfirstest.spec.js
+++ tests/myfirstest.spec.js
@@ -1,9 +1,9 @@
 // const {default:test} = require(@playwirght/test)
 const {test} = require("@playwright/test");
 
-test('My First test', async ()=>{ // "async ()=>" == "async function ()"
+test('My First test', async ({browser})=>{ // "async ()=>" == "async function ()"
     //Playwright Code
-    //Step1 launch 
+    //Step1 launch browser
     await //to be able to use await we need to make this function an async function
     //Step2 Enter Credentials
     //Step3 Click Sign Button
