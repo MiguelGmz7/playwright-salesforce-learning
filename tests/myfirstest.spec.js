9f6575af4c1c1172de0082cd4dabc754e9852390
Miguel Gomez <mail@miguel-gomez.dev>
2026-08-12 02:52:15 -0600

    vault backup: 2026-08-12 02:52:14

--- tests/myfirstest.spec.js
+++ tests/myfirstest.spec.js
@@ -1,10 +1,15 @@
 // const {default:test} = require(@playwirght/test)
 const {test} = require("@playwright/test");
 
-test('My First test', async ({browser})=>{ // "async ()=>" == "async function ()"
+test('My First test', async ({browser})=>{ // "async ()=>" == "async function ()" and we get the browser parameter from playwrigth/test
     //Playwright Code
     //Step1 launch browser
-    browser.newContext();//to be able to use await we need to make this function an async function
+    const context = await browser.newContext(); //Launch the browser and await to open properly
+    const page = await context.newPage(); //Launch the tab and await to open properly
+    page.goto("https://playwright.dev") //Launch the URL
+    
+    //We need to add await so it can be able to AWAIT for instructions to occur, if not
+    // the code will do all at once
     //Step2 Enter Credentials
     //Step3 Click Sign Button
 
