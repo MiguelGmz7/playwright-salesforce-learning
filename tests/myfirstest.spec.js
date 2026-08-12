bc3acd0346d2a65617149232c60d9c6e590023b2
Miguel Gomez <mail@miguel-gomez.dev>
2026-08-12 02:49:44 -0600

    vault backup: 2026-08-12 02:49:44

--- tests/myfirstest.spec.js
+++ tests/myfirstest.spec.js
@@ -4,7 +4,7 @@ const {test} = require("@playwright/test");
 test('My First test', async ({browser})=>{ // "async ()=>" == "async function ()"
     //Playwright Code
     //Step1 launch browser
-    await //to be able to use await we need to make this function an async function
+    browser.newContext();//to be able to use await we need to make this function an async function
     //Step2 Enter Credentials
     //Step3 Click Sign Button
 
