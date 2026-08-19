7fba01e54e33adab18b2cc6118aea47b43a8b357
Miguel Gomez <mail@miguel-gomez.dev>
2026-08-19 15:32:23 -0600

    vault backup: 2026-08-19 15:32:23

--- tests/myfirstest.spec.js
+++ tests/myfirstest.spec.js
@@ -39,8 +39,7 @@ test('Web Automation sauce lab', async({browser})=>{ //Check notebook Notebook\L
     await expect(page).toHaveTitle("Swag Labs");
 
     await page.locator("#user-name").fill("standard_user");
-    await page.locator("#password").fill("secret_sauce");
-    
+    await page.locator("#password").fill("");
     await page.locator("#login-button").click();
     
 });
