23732c7a01bee444cbb652c1e5aae2d2705ed5bd
Miguel Gomez <mail@miguel-gomez.dev>
2026-08-19 16:09:29 -0600

    vault backup: 2026-08-19 16:09:29

--- tests/myfirstest.spec.js
+++ tests/myfirstest.spec.js
@@ -33,7 +33,7 @@ test('Test With Page Context', async ({page})=>{ // "async ()=>" == "async funct
 
 });
 
-test('Error login Web Automation sauce lab', async({browser})=>{ //Check notebook Notebook\Locators.md
+test('Error login Web Automation sauce lab', async({page})=>{ //Check notebook Notebook\Locators.md
     await page.goto("https://www.saucedemo.com/");
     console.log(await page.title);
     await expect(page).toHaveTitle("Swag Labs");
