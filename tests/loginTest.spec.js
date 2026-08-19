1269084f291e646d5b9a736453ab6bfb1126c7ce
Miguel Gomez <mail@miguel-gomez.dev>
2026-08-19 17:10:34 -0600

    vault backup: 2026-08-19 17:10:34

--- tests/loginTest.spec.js
+++ tests/loginTest.spec.js
@@ -1,5 +1,9 @@
 const {test, expect, chromium} = require("@playwright/test");
+let context;
+let page;
 
 test.beforeAll(async() => {
-    
+    context = await chromium.launchPersistentContext(userDataDirectory, {
+        
+    }) 
 });
